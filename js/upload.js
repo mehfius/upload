f49d20db00b7afc85bfc78c8b7722b74e97bafd3
Matheus Ferraz <matheus.ferraz@gmail.com>
2022-07-13 22:34:57 +0000

    teste novo repositorio

--- js/upload.js
+++
@@ -1,9 +0,0 @@
-    const upload = async function(data,filename) {
-      
-      const url="https://php.mehfius.repl.co/upload_v1/";
-      
-      const rawResponse = await fetch(url, {method: 'POST',body:data});
-      
-      return post = await rawResponse.json();
-    
-    }
